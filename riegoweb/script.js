1f6289f66bffc326fd02aa6476796b4b7bfe6821
noelunaa <95946577+noelunaa@users.noreply.github.com>
2022-08-04 07:49:40 -0700

    Add files via upload

--- riegoweb/script.js
+++ riegoweb/script.js
@@ -22,14 +22,14 @@ function onRequestHandler(){
         tabla+="<label style='font-size: xx-large;color:white;'>Fecha y hora:</label><label style='font-size: xx-large; color:lightblue'>  "+ fecha +"</label><br>";
         tabla+="<label style='font-size: xx-large;color:white;'>Humedad:</label><label style='font-size: xx-large; background:black;color:yellow'>  "+ dato +"%</label><br>";
 
-        tabla+="<h2 style='color:white;'>HISTÓRICO DEL SENSOR DE RIEGO</h2>";
-        tabla+="<div style='margin: 0 auto;width: 300px;height: 400px; overflow: auto';>";
+        tabla+="<h2 style='color:white;'>HISTÓRICO DEL SENSOR DE HUMEDAD</h2>";
+        /*tabla+="<div style='margin: 0 auto;width: 300px;height: 400px; overflow: auto';>";
         tabla+="<table  style='margin: 0 auto; '><tr style='background:blue;color:yellow'><td>ID</td><td>FECHA</td><td>DATO</td></tr>"
         for (var i = data.length-1; i >= 0; i-=1) {
             let historial=data[i];
             tabla=tabla+"<tr  style='color:gray; '><td>"+historial.id+"</td><td>"+historial.fecha+"</td><td>"+historial.dato+"</td></tr>";
           }
-          tabla+="</table>";
+          tabla+="</table>";*/
           tabla+="</div>";
 
           const HTMLResponse=document.querySelector('#app');
